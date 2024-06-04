162fa663903c3ecf00690136f8698d03bd8d26c1
Pavan Kumar <rpavankumar@zohomail.in>
2024-06-04 16:15:21 +0530

    s1_10, s2_1

--- script.pl
+++ script.pl
@@ -1,10 +1,10 @@
 #!C:/Strawberry/perl/bin/perl
 
-# Version: 0.0.9
-# Timestamp: 2024-06-04 16:09:38 +0530
+# Version: 0.0.10
+# Timestamp: 2024-06-04 16:15:22 +0530
 # Author: Pavan Kumar
 
-# change 9
+# change 10
 
 use v5.32;
 
