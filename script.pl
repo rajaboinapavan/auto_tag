03ac447e92d2e586ea8f151a1a5a8c0eb7f30fc1
d disk <ddisk@share.com>
2024-06-18 12:55:11 +0530

    yes

--- script.pl
+++ script.pl
@@ -1,10 +1,10 @@
 #!C:/Strawberry/perl/bin/perl
 
-# Version: 0.0.34
-# Timestamp: 2024-06-18 12:53:51 +0530
+# Version: 0.0.35
+# Timestamp: 2024-06-18 12:55:14 +0530
 # Author: d disk
 
-# made change on 32 for 33 #
+# made change on 34 for 35 
 
 use v5.32;
 
