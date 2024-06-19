fb0c136c52c076d5987757d1af608d563b76f2f5
rajaboinapavan <pavanskull@gmail.com>
2024-06-19 20:58:18 +0530

    in 47 for 48

--- script.pl
+++ script.pl
@@ -1,10 +1,10 @@
 #!C:/Strawberry/perl/bin/perl
 
-# Version: 0.0.47
-# Timestamp: 2024-06-19 20:56:10 +0530
+# Version: 0.0.48
+# Timestamp: 2024-06-19 20:58:15 +0530
 # Author: rajaboinapavan
 
-# change from macrosoft 1
+# change in 47 for 48clear
 
 use v5.32;
 
