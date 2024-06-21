14ee5be93a17479ff0cca68444d69875d8972dec
rajaboinapavan <pavanskull@gmail.com>
2024-06-21 18:24:42 +0530

    yes

--- CommunicationsHub/MyModule.pm
+++ CommunicationsHub/MyModule.pm
@@ -1,33 +1,5 @@
 package MyModule;
 
-use strict;
-use warnings;
-
-# Constructor method ###
-sub new {
-    my ($class, %args) = @_;
-    my $self = bless {
-        name => $args{name} || 'Anonymous',
-    }, $class;
-    return $self;
-}
-
-# Method to greet
-sub greet {
-    my ($self) = @_;
-    return "Hello, my name is $self->{name}!";
-}
-
-# Method to set name
-sub set_name {
-    my ($self, $name) = @_;
-    $self->{name} = $name;
-}
-
-# Method to get name
-sub get_name {
-    my ($self) = @_;
-    return $self->{name};
-}
-
-1; # Ensure the module returns true value at the end
+# Version: 0.0.1
+# Timestamp: 2024-06-21 18:24:45 +0530
+# Author: rajaboinapavan
