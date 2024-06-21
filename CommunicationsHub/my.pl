bd4f22b106ed9eff456fdb57307f3c7c94f5423c
rajaboinapavan <pavanskull@gmail.com>
2024-06-21 18:57:20 +0530

    yes

---
+++ CommunicationsHub/my.pl
@@ -0,0 +1,16 @@
+# Version: 0.0.1
+# Timestamp: 2024-06-21 18:57:22 +0530
+# Author: rajaboinapavan
+
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+# Prompt for user's name
+print "Please enter your name: ";
+my $name = <STDIN>;  # Read input from user
+chomp $name;         # Remove newline character from input
+
+# Greet the user
+print "Hello, $name! Nice to meet you.\n";
