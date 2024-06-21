8e5ee19290b96a7d7c2e0609a256900c3860302f
rajaboinapavan <pavanskull@gmail.com>
2024-06-21 18:16:42 +0530

    yes

--- CommunicationsHub/MyModule.pm
+++ CommunicationsHub/MyModule.pm
@@ -3,7 +3,7 @@ package MyModule;
 use strict;
 use warnings;
 
-# Constructor method
+# Constructor method #
 sub new {
     my ($class, %args) = @_;
     my $self = bless {
@@ -31,47 +31,3 @@ sub get_name {
 }
 
 1; # Ensure the module returns true value at the end
-
-__END__
-
-=head1 NAME
-
-MyModule - Example Perl Module
-
-=head1 SYNOPSIS
-
-  use MyModule;
-
-  my $obj = MyModule->new(name => 'Alice');
-  print $obj->greet(), "\n";       # Output: Hello, my name is Alice!
-  $obj->set_name('Bob');
-  print $obj->greet(), "\n";       # Output: Hello, my name is Bob!
-
-=head1 DESCRIPTION
-
-MyModule is a simple Perl module demonstrating basic object-oriented programming concepts.
-
-=head1 METHODS
-
-=head2 new(%args)
-
-Constructor method to create a new MyModule object. Accepts an optional 'name' parameter.
-
-=head2 greet()
-
-Returns a greeting message using the current object's name attribute.
-
-=head2 set_name($name)
-
-Sets the name attribute of the object.
-
-=head2 get_name()
-
-Returns the current name attribute of the object.
-
-=head1 AUTHOR
-
-Your Name
-
-=cut
-
